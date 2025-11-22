addf78afda5b9ece59ac91fe6e571e6c05887153
Hanjae Lee <jayhanjaelee@gmail.com>
2025-11-22 19:25:57 +0900

    Add delete user function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { useEffect, useState } from 'react';
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, CanceledError } from 'axios';
 
 interface User {
   id: string;
@@ -13,52 +13,53 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          'https://jsonplaceholder.typicode.com/usersx'
-        );
+    const controller = new AbortController();
+
+    setLoading(true);
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
+      .then((res) => {
         setUsers(res.data);
-
-        console.log(res);
-      } catch (err) {
-        console.log(err);
-        setError((err as AxiosError).message);
-      }
-    };
-
-    fetchUsers();
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios.delete('https://jsonplaceholder.typicode.com/users/' + user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
     <>
-      <div className="container">
+      <div className="container mt-5">
         {error && <p className="text-danger">{error}</p>}
-        {users.length > 1 && (
-          <table>
-            <thead>
-              <th>id</th>
-              <th>name</th>
-              <th>username</th>
-              <th>email</th>
-            </thead>
-
-            <tbody>
-              {users.map((user, i) => {
-                return (
-                  <tr>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.username}</td>
-                    <td>{user.email}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
+        {isLoading && <div className="spinner-border"> </div>}
+        <ul className="list-group">
+          {users.map((user) => (
+            <li key={user.id} className="list-group-item d-flex justify-content-between">
+              {user.name}
+              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
